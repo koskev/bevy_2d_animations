10307ee1fd97c5ad2efd268341b107a9a7f71785
Kevin <kevin@kokev.de>
2024-03-29 16:26:49 +0100

    Add event to bevy (would be nice to have this as a compile time check ;) )

--- src/lib.rs
+++ src/lib.rs
@@ -107,6 +107,7 @@ pub struct AnimationPlugin;
 
 impl Plugin for AnimationPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, update_animations);
+        app.add_systems(Update, update_animations)
+            .add_event::<AnimationFinishedEvent>();
     }
 }
